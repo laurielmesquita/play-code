fc95fb2a044dd5ee1154b1f53af51e99a40556e7
Lauriel Mesquita <laurielmesquita@me.com>
2020-01-04 23:24:29 -0300

    Change some things

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `Brincadeira de Criança`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `Quer frango?`,
+    author: `@brincadeira`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
