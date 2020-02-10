cb670cf7c2b3a0093ff9f1b1de76703dbdd63d85
Lauriel Mesquita <laurielmesquita@me.com>
2020-02-09 21:55:45 -0300

    More configs again

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+const postcssPresetEnv = require('postcss-preset-env')
+
 module.exports = {
   siteMetadata: {
     title: 'Brincadeira de Criança',
@@ -6,6 +8,7 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-react-helmet',
+    'gatsby-transformer-yaml',
     {
       resolve: 'gatsby-plugin-prefetch-google-fonts',
       options: {
@@ -18,14 +21,87 @@ module.exports = {
       }
     },
     {
+      resolve: 'gatsby-plugin-offline',
+      options: {
+        runtimeCaching: [
+          {
+            // Use cacheFirst since these don't need to be revalidated (same RegExp
+            // and same reason as above)
+            urlPattern: /(\.js$|\.css$|static\/)/,
+            handler: 'cacheFirst'
+          },
+          {
+            // Add runtime caching of various other page resources
+            urlPattern: /^https?:.*\.(png|jpg|jpeg|webp|svg|gif|tiff|js|woff|woff2|json|css)$/,
+            handler: 'staleWhileRevalidate'
+          }
+        ],
+        skipWaiting: true,
+        clientsClaim: true
+      }
+    },
+    {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'images',
         path: `${__dirname}/static/images`
       }
     },
-    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/content`,
+        name: 'pages'
+      }
+    },
     'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          // gatsby-remark-relative-images must
+          // go before gatsby-remark-images
+          'gatsby-remark-relative-images',
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 800,
+              linkImagesToOriginal: false
+            }
+          },
+          'gatsby-remark-responsive-iframe'
+        ]
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-sass',
+      options: {
+        postCssPlugins: [
+          postcssPresetEnv({
+            browsers: '> 0.5%, last 2 versions, ie 11'
+          })
+        ]
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-postcss',
+      options: {
+        postCssPlugins: [
+          require('postcss-preset-env')({
+            browsers: '> 0.5%, last 2 versions, ie 11'
+          })
+        ]
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-nprogress',
+      options: {
+        color: 'black',
+        showSpinner: true
+      }
+    },
+    'gatsby-plugin-sitemap',
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
@@ -37,7 +113,7 @@ module.exports = {
         display: 'standalone',
         icon: `${__dirname}/static/images/icon.png`
       }
-    }
-    // 'gatsby-plugin-offline'
+    },
+    'gatsby-plugin-netlify'
   ]
 }
