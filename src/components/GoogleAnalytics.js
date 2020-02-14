aa98cf4f546eaf92d90264721e0560b27b4e94e4
Lauriel Mesquita <laurielmesquita@me.com>
2020-02-14 14:22:12 -0300

    Change the Google Analytics code

--- src/components/GoogleAnalytics.js
+++ src/components/GoogleAnalytics.js
@@ -6,7 +6,6 @@ export default () => (
   <Route
     path='/'
     render={({ location }) => {
-      // Assumes google analytics code already added to index.html
       if (typeof window.ga === 'function') {
         window.ga('set', 'page', location.pathname + location.search)
         window.ga('send', 'pageview')
