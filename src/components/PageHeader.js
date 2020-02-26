5b4fa389d7d2598106ba40980a13bcf4ea2adbbc
Lauriel Mesquita <laurielmesquita@me.com>
2020-02-26 00:15:33 -0300

    :sparkles: New features
    Introducing the header component

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -1,35 +1,16 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import Image from './Image'
-import Content from './Content'
-
 const PageHeader = ({
   title,
-  subtitle,
-  backgroundImage,
   large,
   className = ''
 }) => {
   if (large) className += ' PageHeader-large'
   return (
     <div className={`PageHeader relative ${className}`}>
-      {backgroundImage && (
-        <Image
-          background
-          resolutions="large"
-          src={backgroundImage}
-          alt={title}
-          size="cover"
-        />
-      )}
       <div className="container relative">
-        <div className="PageHeader--Item">
-          <h1 className="PageHeader--Title">{title}</h1>
-          {subtitle && (
-            <Content className="PageHeader--Subtitle" src={subtitle} />
-          )}
-        </div>
+        <h1 className="PageHeader--Title">{title}</h1>
       </div>
     </div>
   )
